820dacbfedc22f250228bcba4b9de9b8309166b9
Yordi Ogando <yordiogando@gmail.com>
2020-10-27 18:00:23 -0400

    Implementation of crerate lesson done

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,14 @@
-import { Query } from "@nestjs/graphql";
+import { Args, Mutation, Query } from "@nestjs/graphql";
 import { Resolver } from "@nestjs/graphql";
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver(of => LessonType)//This resolver is of type "LessonType"
 export class LessonResolver {
+    constructor(
+        private lessonService: LessonService
+    ) { }
+
     @Query(returns => LessonType)//Here we define what type does this Query returns
     lesson() {
         return {
@@ -12,5 +17,14 @@ export class LessonResolver {
             startDate: (new Date()).toISOString(),
             endDate: (new Date()).toISOString(),
         }
+    }
+
+    @Mutation(returns => LessonType)
+    createLesson(
+        @Args('name') name: string,
+        @Args('startDate') startDate: string,
+        @Args('endDate') endDate: string,
+    ) {
+        return this.lessonService.createLesson(name, startDate, endDate)
     }
 }
--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -1,6 +1,6 @@
 import { Field, ID, ObjectType } from "@nestjs/graphql";
 
-@ObjectType('Lesson')//This is the name of the type
+@ObjectType('Lesson')//This is the name of the GraphQL type
 export class LessonType {
     @Field(type => ID)
     id: string;

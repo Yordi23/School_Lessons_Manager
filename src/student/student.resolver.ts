b783c7f8eb7c87327fa83dba23d80a9da9f50a08
Yordi Ogando <yordiogando@gmail.com>
2020-10-30 19:17:53 -0400

    Create student and get all students implemented

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,24 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Student } from './student.entity';
+import { StudentService } from './student.service';
+import { StudentType } from './student.type';
+import { CreateStudentInput } from './create-student.input';
+
+@Resolver(of => StudentType)
+export class StudentResolver {
+    constructor(
+        private studentService: StudentService
+    ) { }
+
+    @Query(returns => [StudentType])
+    students() {
+        return this.studentService.getStudents();
+    }
+
+    @Mutation(returns => StudentType)
+    createStudent(
+        @Args('createStudentInput') createStudentInput: CreateStudentInput
+    ) {
+        return this.studentService.createStudent(createStudentInput);
+    }
+}

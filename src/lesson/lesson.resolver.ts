155ae58163eea9e137c4f83c33593d07f88804c9
Yordi Ogando <yordiogando@gmail.com>
2020-10-31 13:48:10 -0400

    Resolver "students" field in lessons

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,14 +1,17 @@
-import { Args, Mutation, Query } from "@nestjs/graphql";
+import { Args, Mutation, Parent, Query, ResolveField } from "@nestjs/graphql";
 import { Resolver } from "@nestjs/graphql";
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from "./lesson.input";
 import { AssignStudentsToLessonInput } from "./assign-students-to-lesson.input";
+import { Lesson } from './lesson.entity';
+import { StudentService } from '../student/student.service';
 
 @Resolver(of => LessonType)//This resolver is of type "LessonType"
 export class LessonResolver {
     constructor(
-        private lessonService: LessonService
+        private lessonService: LessonService,
+        private studentService: StudentService
     ) { }
 
     @Query(returns => LessonType)//Here we define what type does this Query returns
@@ -43,6 +46,11 @@ export class LessonResolver {
         const { lessonId, studentIds } = assignStudentsToLessonInput;
 
         return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+    }
+
+    @ResolveField()
+    async students(@Parent() lesson: Lesson) {
+        return this.studentService.getManyStudents(lesson.students)
     }
 
 }
